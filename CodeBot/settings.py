cf8800557a1e3964c9a166941b5cd43cfdc06001
TanushriTarale <tanushritarale@gmail.com>
2024-09-07 16:28:41 +0530

    done vercel setting

--- CodeBot/settings.py
+++ CodeBot/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-w0bf@_j2l0iqt@1v)#fz&gt6__2!lohg&4#t&e@el$e_owbx0y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['.vercel.app' , '.now.sh']
 
 # Application definition
 
@@ -122,3 +121,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
